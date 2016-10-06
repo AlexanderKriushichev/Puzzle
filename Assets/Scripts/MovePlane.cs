2cb3de2fca9d7fbe3a3a5d0c27579b4393ec8971
TurusovEvgeny <turusov.evg@gmail.com>
2016-10-06 09:20:54 +0500

    Add script MovePlane

---
+++ Assets/Scripts/MovePlane.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlane : MonoBehaviour
+{
+    public float speed = 0.005f;
+    public Vector2 direction = new Vector2(-1, 0);
+
+    void Update()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+    }    
+}
